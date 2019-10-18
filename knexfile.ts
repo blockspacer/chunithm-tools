29d7aa7b947c96313a833407f71209d3b9ed9b6f
Raclette <im@s283.net>
2019-10-18 12:17:31 +0900

    [Modify] General: knexfileの形式を変更

--- knexfile.ts
+++ knexfile.ts
@@ -3,70 +3,68 @@ import * as Config from "./src/config";
 Config.loadConfig(process.cwd() + "/config/config.json");
 const config = Config.getConfig();
 
-module.exports = {
-    development: {
-        client: "mysql",
-        connection: {
-            port: config.dbPort,
-            host: config.dbHost,
-            database: config.dbName,
-            user: config.dbUser,
-            password: config.dbPassword
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: "./db/migrations",
-            tableName: "knex_migrations"
-        },
-        seeds: {
-            directory: "./db/seeds",
-        },
+export const development = {
+    client: "mysql",
+    connection: {
+        port: config.dbPort,
+        host: config.dbHost,
+        database: config.dbName,
+        user: config.dbUser,
+        password: config.dbPassword
     },
-
-    staging: {
-        client: "mysql",
-        connection: {
-            port: config.dbPort,
-            host: config.dbHost,
-            database: config.dbName,
-            user: config.dbUser,
-            password: config.dbPassword
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: "./db/migrations",
-            tableName: "knex_migrations"
-        },
-        seeds: {
-            directory: "./db/seeds",
-        },
+    pool: {
+        min: 2,
+        max: 10
     },
+    migrations: {
+        directory: "./db/migrations",
+        tableName: "knex_migrations"
+    },
+    seeds: {
+        directory: "./db/seeds",
+    }
+};
 
-    production: {
-        client: "mysql",
-        connection: {
-            port: config.dbPort,
-            host: config.dbHost,
-            database: config.dbName,
-            user: config.dbUser,
-            password: config.dbPassword
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: "./db/migrations",
-            tableName: "knex_migrations"
-        },
-        seeds: {
-            directory: "./db/seeds",
-        },
+export const staging = {
+    client: "mysql",
+    connection: {
+        port: config.dbPort,
+        host: config.dbHost,
+        database: config.dbName,
+        user: config.dbUser,
+        password: config.dbPassword
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        directory: "./db/migrations",
+        tableName: "knex_migrations"
+    },
+    seeds: {
+        directory: "./db/seeds",
+    }
+};
+
+export const production = {
+    client: "mysql",
+    connection: {
+        port: config.dbPort,
+        host: config.dbHost,
+        database: config.dbName,
+        user: config.dbUser,
+        password: config.dbPassword
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        directory: "./db/migrations",
+        tableName: "knex_migrations"
+    },
+    seeds: {
+        directory: "./db/seeds",
     }
 };
