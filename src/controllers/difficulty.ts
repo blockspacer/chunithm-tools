6290b58d2b7affecc6520e61fc0138a82e0d301f
Raclette <im@s283.net>
2019-10-21 23:21:04 +0900

    [Add] Server: リコメンドの追加

--- src/controllers/difficulty.ts
+++ src/controllers/difficulty.ts
@@ -1,9 +1,11 @@
 import {knex, onDuplicateKey} from "../knex_wrapper";
 import {Difficulty} from "../models/difficulty";
+import {Recommend} from "../models/recommend";
 
 export async function cacheDifficulty() {
     const songIdList = (await knex("songs")
-                                .select("songid"))
+                                .select("songid")
+                                .where("difficulty", Difficulty.MASTER))
                             .map((row) => Number(row.songid));
 
     const ratesCount = (await knex("players")
@@ -29,6 +31,10 @@ export async function cacheDifficulty() {
                                 .andWhereBetween("score", [900000, 1010000])
                                 .orderBy("score", "desc");
         const scoresCount = scores.length;
+
+        if (scoresCount === 0) {
+            continue;
+        }
 
         for (let rate = 1600; rate >= 1300; rate--) {
             if (!(rate in ratesCount)) {
@@ -63,4 +69,61 @@ export async function getDifficulty(songId: number, score: number) {
     }
 
     return 1298;
+}
+
+type Options = {
+    count?: number,
+    minRateValue?: number,
+    maxRateValue?: number
+};
+
+export async function getRecommend(playerId: string, options: Options = {}): Promise<Recommend[]> {
+    const rows = await knex("songs")
+                        .innerJoin("scores", function() {
+                            this.on("songs.difficulty", "scores.difficulty");
+                            this.andOn("songs.songid", "scores.songid");
+                        })
+                        .innerJoin("difficulty", "songs.songid", "difficulty.songid")
+                        .select(
+                            "songs.songid",
+                            "songs.songname",
+                            "songs.difficulty",
+                            "songs.ratevalue",
+                            "songs.notes",
+                            "songs.scorevideourl",
+                            "songs.scoreimageurl",
+                            "songs.genreid")
+                        .max("difficulty.rate as rate")
+                        .where("scores.playerid", playerId)
+                        .andWhereRaw("scores.score >= difficulty.score")
+                        .andWhereBetween("songs.ratevalue", [options.minRateValue || 10, options.maxRateValue || 160])
+                        .groupBy(
+                            "songs.songid",
+                            "songs.songname",
+                            "songs.difficulty",
+                            "songs.ratevalue",
+                            "songs.notes",
+                            "songs.scorevideourl",
+                            "songs.scoreimageurl",
+                            "songs.genreid")
+                        .orderByRaw(
+                            "max(rate) " + (
+                                options.count
+                                ? options.count >= 0 ? "asc" : "desc"
+                                : "asc"))
+                        .limit(Math.abs(options.count || 999));
+
+    return rows.map((row) => ({
+        song: {
+            songId: Number(row.songid),
+            songName: String(row.songname),
+            difficulty: Number(row.difficulty),
+            rateValue: Number(row.ratevalue),
+            notes: Number(row.notes),
+            scoreVideo: String(row.scorevideourl),
+            scoreImage: String(row.scoreimageurl),
+            genreId: Number(row.genreid)
+        },
+        rate: Number(row.rate)
+    }));
 }
--- test/controllers/difficulty.test.ts
+++ test/controllers/difficulty.test.ts
@@ -1,20 +1,36 @@
-import * as Difficulty from "../../src/controllers/difficulty";
+import * as DifficultyModel from "../../src/controllers/difficulty";
 import {setPlayer} from "../../src/controllers/players";
 import {setScores} from "../../src/controllers/scores";
 import {createSong} from "../../src/controllers/songs";
 import {knex} from "../../src/knex_wrapper";
+import {Difficulty} from "../../src/models/difficulty";
+import {ScoreList} from "../../src/models/score_list";
 
 describe("曲の適正難易度の算出", () => {
     beforeAll(() => {
-        const playerRates = [1525, 1550];
-        const scores = [1005000, 1007500];
+        const playerRates = [1500, 1525, 1550];
+        const scores: ScoreList[] = [
+            {
+                1: [[0, 0], [0, 0], [0, 0], [1005000, 0]],
+                2: [[0, 0], [0, 0], [0, 0], [1007000, 0]],
+                3: [[0, 0], [0, 0], [0, 0], [1009000, 0]]
+            }, {
+                1: [[0, 0], [0, 0], [0, 0], [1007000, 0]],
+                2: [[0, 0], [0, 0], [0, 0], [1009000, 0]],
+                3: [[0, 0], [0, 0], [0, 0], [1005000, 0]]
+            }, {
+                1: [[0, 0], [0, 0], [0, 0], [1009000, 0]],
+                2: [[0, 0], [0, 0], [0, 0], [1005000, 0]],
+                3: [[0, 0], [0, 0], [0, 0], [1007000, 0]]
+            }
+        ];
 
         return Promise.all([
             ...playerRates.map((rate, index) => setPlayer(String(index), "", rate, rate, "", 0, 0)),
-            ...scores.map((score, index) => setScores(String(index), {
-                1: [[0, 0], [0, 0], [0, 0], [score, 0]]
-            })),
-            createSong(1, "", 0, 0, 0, 0, 0, "", "", 0)
+            ...scores.map((score, index) => setScores(String(index), score)),
+            createSong(1, "", 0, 0, 0, 100, 0, "", "", 0),
+            createSong(2, "", 0, 0, 0, 110, 0, "", "", 0),
+            createSong(3, "", 0, 0, 0, 120, 0, "", "", 0)
         ]);
     });
 
@@ -22,31 +38,126 @@ describe("曲の適正難易度の算出", () => {
         return Promise.all([
             knex("players").del(),
             knex("songs").del(),
+            knex("scores").del(),
             knex("difficulty").del()
         ]);
     });
 
     test("適正難易度のキャッシュ", () => {
-        return expect(Difficulty.cacheDifficulty())
+        return expect(DifficultyModel.cacheDifficulty())
                 .resolves
                 .toBeUndefined();
     });
 
     test("適正難易度の取得", () => {
         return expect(Promise.all([
-                    Difficulty.getDifficulty(1, 1008000),
-                    Difficulty.getDifficulty(1, 1007500),
-                    Difficulty.getDifficulty(1, 1006000),
-                    Difficulty.getDifficulty(1, 1005000),
-                    Difficulty.getDifficulty(1, 1004000)
-                ]))
-                .resolves
-                .toMatchObject([
-                    1550,
-                    1550,
-                    1525,
-                    1525,
-                    1298
-                ]);
+                DifficultyModel.getDifficulty(1, 1010000),
+                DifficultyModel.getDifficulty(1, 1009000),
+                DifficultyModel.getDifficulty(1, 1008000),
+                DifficultyModel.getDifficulty(1, 1007000),
+                DifficultyModel.getDifficulty(1, 1006000),
+                DifficultyModel.getDifficulty(1, 1005000),
+                DifficultyModel.getDifficulty(1, 1004000)
+            ]))
+            .resolves
+            .toMatchObject([
+                1550,
+                1550,
+                1525,
+                1525,
+                1500,
+                1500,
+                1298
+            ]);
+    });
+
+    test("リコメンドの取得", () => {
+        return expect(Promise.all([
+                DifficultyModel.getRecommend("0"),
+                DifficultyModel.getRecommend("1", {count: 2}),
+                DifficultyModel.getRecommend("2", {minRateValue: 110, maxRateValue: 110}),
+            ]))
+            .resolves
+            .toMatchObject([
+                [
+                    {
+                        song: {
+                            songId: 1,
+                            songName: "",
+                            difficulty: Difficulty.MASTER,
+                            rateValue: 100,
+                            notes: 0,
+                            scoreVideo: "",
+                            scoreImage: "",
+                            genreId: 0
+                        },
+                        rate: 1500,
+                    }, {
+                        song: {
+                            songId: 2,
+                            songName: "",
+                            difficulty: Difficulty.MASTER,
+                            rateValue: 110,
+                            notes: 0,
+                            scoreVideo: "",
+                            scoreImage: "",
+                            genreId: 0
+                        },
+                        rate: 1525,
+                    }, {
+                        song: {
+                            songId: 3,
+                            songName: "",
+                            difficulty: Difficulty.MASTER,
+                            rateValue: 120,
+                            notes: 0,
+                            scoreVideo: "",
+                            scoreImage: "",
+                            genreId: 0
+                        },
+                        rate: 1550,
+                    }
+                ], [
+                    {
+                        song: {
+                            songId: 3,
+                            songName: "",
+                            difficulty: Difficulty.MASTER,
+                            rateValue: 120,
+                            notes: 0,
+                            scoreVideo: "",
+                            scoreImage: "",
+                            genreId: 0
+                        },
+                        rate: 1500,
+                    }, {
+                        song: {
+                            songId: 1,
+                            songName: "",
+                            difficulty: Difficulty.MASTER,
+                            rateValue: 100,
+                            notes: 0,
+                            scoreVideo: "",
+                            scoreImage: "",
+                            genreId: 0
+                        },
+                        rate: 1525,
+                    }
+                ], [
+                    {
+                        song: {
+                            songId: 2,
+                            songName: "",
+                            difficulty: Difficulty.MASTER,
+                            rateValue: 110,
+                            notes: 0,
+                            scoreVideo: "",
+                            scoreImage: "",
+                            genreId: 0
+                        },
+                        rate: 1500,
+                    }
+                ]
+            ]);
     });
 });
