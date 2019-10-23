6cab6862f08b89c3622482bfeb3e7fdd437ae005
Raclette <im@s283.net>
2019-10-23 23:21:56 +0900

    [Fix] Server: 例外を捕捉できないAPIを修正

--- src/routes/api/players/ratelog.ts
+++ src/routes/api/players/ratelog.ts
@@ -2,7 +2,7 @@ import * as Express from "express";
 import {getRateLog} from "../../../controllers/players";
 import {decodeJWT} from "../../../helper/jwt";
 
-export default async function(req: Express.Request, res: Express.Response): Promise<void> {
+export default async function(req: Express.Request, res: Express.Response, next: Express.NextFunction): Promise<void> {
     const token: string = req.body.token;
 
     if (
@@ -16,7 +16,7 @@ export default async function(req: Express.Request, res: Express.Response): Prom
         const playerId = await decodeJWT(token);
         const rateLog = getRateLog(playerId);
         res.status(200).json({status: "SUCCESS", rateLog});
-    } catch {
-        res.status(200).json({status: "FAILED"});
+    } catch (err) {
+        next(err);
     }
 }
