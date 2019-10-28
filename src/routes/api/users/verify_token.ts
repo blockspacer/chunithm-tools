b51c68f998e389fdfa4171ee39ef265d95a48fdf
Raclette <im@s283.net>
2019-10-28 11:46:20 +0900

    [Add] Server: トークンを検証するAPIを追加

--- src/routes/api/users/verify_token.ts
+++ src/routes/api/users/verify_token.ts
@@ -1,6 +1,5 @@
 import * as Express from "express";
-import {getPlayerId} from "../../../controllers/users";
-import {ControllerError} from "../../../exceptions";
+import {decodeJWT} from "../../../helper/jwt";
 
 export default async function(req: Express.Request, res: Express.Response, next: Express.NextFunction): Promise<void> {
     const token: string = req.body.token;
@@ -13,10 +12,10 @@ export default async function(req: Express.Request, res: Express.Response, next:
     }
 
     try {
-        await getPlayerId(token);
+        await decodeJWT(token);
         res.status(200).json({status: "SUCCESS"});
     } catch (err) {
-        if (err instanceof ControllerError) {
+        if (err instanceof Error) {
             res.status(200).json({status: "FAILED"});
         } else {
             next(err);
