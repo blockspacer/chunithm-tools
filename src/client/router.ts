e3aff595d3f80ba98769f9e9ed28c210a84cb4e9
Raclette <im@s283.net>
2019-11-21 11:27:38 +0900

    [Add] Client: サインアップページの追加

--- src/client/router.ts
+++ src/client/router.ts
@@ -2,12 +2,16 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import NotFound from "./views/NotFound.vue";
+import Signin from "./views/Signin.vue";
+import Signup from "./views/Signup.vue";
 
 Vue.use(Router);
 
 const router = new Router({
     routes: [
         {path: "/", component: Home},
+        {path: "/signin", component: Signin},
+        {path: "/signup", component: Signup},
         {path: "*", component: NotFound}
     ]
 });
