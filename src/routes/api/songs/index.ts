4709c240a5a4050e69f34b6a6c2eb2670d74048f
Raclette <im@s283.net>
2019-11-22 00:58:37 +0900

    [Add] Server: 適正難易度APIを追加

--- src/routes/api/songs/index.ts
+++ src/routes/api/songs/index.ts
@@ -1,9 +1,11 @@
 import * as Express from "express";
+import difficulty from "./difficulty";
 import random from "./random";
 import search from "./search";
 
 const router = Express.Router();
 
+router.post("/difficulty", difficulty);
 router.post("/random", random);
 router.post("/search", search);
 
