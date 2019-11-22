a09063c2952dbef8ef02c7c770c4cfa3e2e94753
Raclette <im@s283.net>
2019-11-22 16:14:12 +0900

    [Modify] General: インポートの形式の変更

--- test/helper/formatter.test.ts
+++ test/helper/formatter.test.ts
@@ -1,14 +1,14 @@
-import {difficultyToInteger, integerToRate, integerToRateValue, integerToScoreMark, levelToInteger, rateValueToInteger, scoreToInteger} from "../../src/helper/formatter";
+import * as Formatter from "../../src/helper/formatter";
 
 describe("フォーマット関数", () => {
     test("スコア文字列から整数への変換", () => {
         expect([
-                scoreToInteger("SSS"),
-                scoreToInteger("S+"),
-                scoreToInteger("1007500"),
-                scoreToInteger("1006"),
-                scoreToInteger("9960"),
-                scoreToInteger("1002k"),
+                Formatter.scoreToInteger("SSS"),
+                Formatter.scoreToInteger("S+"),
+                Formatter.scoreToInteger("1007500"),
+                Formatter.scoreToInteger("1006"),
+                Formatter.scoreToInteger("9960"),
+                Formatter.scoreToInteger("1002k"),
             ])
             .toMatchObject([
                 1007500,
@@ -22,8 +22,8 @@ describe("フォーマット関数", () => {
 
     test("整数からレートへの変換", () => {
         expect([
-                integerToRate(1234),
-                integerToRate(100)
+                Formatter.integerToRate(1234),
+                Formatter.integerToRate(100)
             ])
             .toMatchObject([
                 "12.34",
@@ -33,9 +33,9 @@ describe("フォーマット関数", () => {
 
     test("整数から譜面定数への変換", () => {
         expect([
-                integerToRateValue(124),
-                integerToRateValue(10),
-                integerToRateValue(130)
+                Formatter.integerToRateValue(124),
+                Formatter.integerToRateValue(10),
+                Formatter.integerToRateValue(130)
             ])
             .toMatchObject([
                 "12.4",
@@ -46,8 +46,8 @@ describe("フォーマット関数", () => {
 
     test("譜面定数から整数への変換", () => {
         expect([
-                rateValueToInteger("12.3"),
-                rateValueToInteger("14")
+                Formatter.rateValueToInteger("12.3"),
+                Formatter.rateValueToInteger("14")
             ])
             .toMatchObject([
                 123,
@@ -57,8 +57,8 @@ describe("フォーマット関数", () => {
 
     test("難易度から整数範囲への変換", () => {
         expect([
-                levelToInteger("12"),
-                levelToInteger("13+")
+                Formatter.levelToInteger("12"),
+                Formatter.levelToInteger("13+")
             ])
             .toMatchObject([
                 {min: 120, max: 126},
@@ -68,9 +68,9 @@ describe("フォーマット関数", () => {
 
     test("難易度または譜面定数から整数範囲への変換", () => {
         expect([
-                difficultyToInteger("12"),
-                difficultyToInteger("13+"),
-                difficultyToInteger("13.5")
+                Formatter.difficultyToInteger("12"),
+                Formatter.difficultyToInteger("13+"),
+                Formatter.difficultyToInteger("13.5")
             ])
             .toMatchObject([
                 {min: 120, max: 126},
@@ -81,10 +81,10 @@ describe("フォーマット関数", () => {
 
     test("整数からスコアランプへの変換", () => {
         expect([
-                integerToScoreMark(0),
-                integerToScoreMark(2),
-                integerToScoreMark(4),
-                integerToScoreMark(6)
+                Formatter.integerToScoreMark(0),
+                Formatter.integerToScoreMark(2),
+                Formatter.integerToScoreMark(4),
+                Formatter.integerToScoreMark(6)
             ])
             .toMatchObject([
                 "",
