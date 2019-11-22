ceb7f0c385ecd11e5b6838c42f268188887a5e33
Raclette <im@s283.net>
2019-11-22 18:15:18 +0900

    [Add] Server: リコメンド曲のAPIを追加

--- src/routes/api/scores/index.ts
+++ src/routes/api/scores/index.ts
@@ -3,6 +3,7 @@ import best from "./best";
 import difference from "./difference";
 import getHistory from "./get_history";
 import getScores from "./get_scores";
+import recommend from "./recommend";
 import setHistory from "./set_history";
 import setScores from "./set_scores";
 
@@ -14,5 +15,6 @@ router.post("/get_history", getHistory);
 router.post("/get_scores", getScores);
 router.post("/set_history", setHistory);
 router.post("/set_scores", setScores);
+router.post("/recommend", recommend);
 
 export default router;
