7c79ef7625b7a8211ee5ed5c399d9d4f844bca92
Raclette <im@s283.net>
2019-11-22 22:32:02 +0900

    [Modify] General: リコメンドに使用するスコアを追加

--- db/seeds/prod/difficulty.ts
+++ db/seeds/prod/difficulty.ts
@@ -6,11 +6,25 @@ export async function seed(knex: Knex): Promise<any> {
         {songid: 1, rate: 1550, score: 1008000},
         {songid: 1, rate: 1525, score: 1007000},
         {songid: 1, rate: 1500, score: 1006000},
+        {songid: 1, rate: 1475, score: 1005000},
+        {songid: 1, rate: 1450, score: 1004000},
+        {songid: 1, rate: 1425, score: 1003000},
+        {songid: 1, rate: 1400, score: 1002000},
+
         {songid: 2, rate: 1550, score: 1009000},
         {songid: 2, rate: 1525, score: 1008000},
         {songid: 2, rate: 1500, score: 1007000},
+        {songid: 2, rate: 1475, score: 1006000},
+        {songid: 2, rate: 1450, score: 1005000},
+        {songid: 2, rate: 1425, score: 1004000},
+        {songid: 2, rate: 1400, score: 1003000},
+
         {songid: 3, rate: 1550, score: 1007000},
         {songid: 3, rate: 1525, score: 1006000},
-        {songid: 3, rate: 1500, score: 1005000}
+        {songid: 3, rate: 1500, score: 1005000},
+        {songid: 3, rate: 1475, score: 1004000},
+        {songid: 3, rate: 1450, score: 1003000},
+        {songid: 3, rate: 1425, score: 1002000},
+        {songid: 3, rate: 1400, score: 1001000}
     ]);
 }
