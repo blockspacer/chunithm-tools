02d3fa806625fa68d90e325f7d032e3feff80113
Raclette <im@s283.net>
2019-11-25 12:11:50 +0900

    [Add] General: WORLD'S Endのスコア一覧モデルを追加

--- src/models/worldsendscore_list.ts
+++ src/models/worldsendscore_list.ts
@@ -1,3 +1,20 @@
 export type WorldsEndScoreList = {
     [key: number]: [number, number]
 };
+
+export function validateWorldsEndScoreList(scoreList: WorldsEndScoreList) {
+    for (const songId of Object.keys(scoreList)) {
+        if (Number.isNaN(Number(songId))) {
+            return false;
+        }
+
+        if (
+            typeof scoreList[Number(songId)][0] === "number"
+            && typeof scoreList[Number(songId)][1] === "number"
+        ) {
+            return false;
+        }
+    }
+
+    return true;
+}
