ba943ef87e1ef99087141f54d0c385a6b515ba63
Raclette <im@s283.net>
2019-12-15 15:11:12 +0900

    [Modify] General: URLを本番環境のものに変更

--- src/routes/api/users/linelogin.ts
+++ src/routes/api/users/linelogin.ts
@@ -4,5 +4,5 @@ import {getConfig} from "../../../config";
 export default async function(_: Express.Request, res: Express.Response): Promise<void> {
     const config = getConfig();
     res.redirect(302,
-        "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=" + config.lineLogin.clientId + "&redirect_uri=https%3A%2F%2Fctdev.raclett3.com%2Flinelogin&state=chunithmtools&scope=openid");
+        "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=" + config.lineLogin.clientId + "&redirect_uri=https%3A%2F%2Fchunithmtools.net%2Flinelogin&state=chunithmtools&scope=openid");
 }
